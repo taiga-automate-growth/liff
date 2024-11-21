36eced16fae070ae998b9af528527fc457b2974c
taiga-automate-growth <oshima@automate-growth.com>
2024-11-22 05:56:43 +0900

    update: ガイド一覧に変更

--- liff/js/guide/edit.js
+++ liff/js/guide/edit.js
@@ -2,7 +2,7 @@ import { editGuideId } from "../../env.js";
 import { initLiff } from "../liff.js";
 
 const liffId = editGuideId;
-const query  = "?path=guides/edit";
+const query  = "?path=guides";
 const accountType = "guide";
 
 initLiff(liffId, query, accountType);
